6256ebd195ea7c040e9e3edcb61414ab486363d4
airburst <mark@fairhursts.net>
2018-08-22 18:23:15 +0100

    feat: Added search box to MediaViewer

--- src/components/Manager/MediaViewer/index.js
+++ src/components/Manager/MediaViewer/index.js
@@ -1,92 +1,118 @@
 import React from 'react';
 import './MediaViewer.css';
 
-const MediaViewer = () => (
-  <div>
-    <div className="topbar">&nbsp;</div>
-    <div className="media-container">
-      <div className="media">
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
+class MediaViewer extends React.Component {
+  state = {
+    search: '',
+  };
+
+  onChange = (name, e) => {
+    this.setState({ [name]: e.target.value });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <div>
+        <div className="topbar">
+          <div className="left" />
+          <div className="right">
+            <input
+              className="search-input"
+              type="text"
+              value={search}
+              name="search"
+              placeholder="Search"
+              onChange={this.onChange.bind(null, 'search')}
+            />
+          </div>
         </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/100x150"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/100x150"
-            alt="Placeholder"
-          />
-        </div>
-        <div className="thumbnail">
-          <img
-            src="https://source.unsplash.com/random/150x100"
-            alt="Placeholder"
-          />
+        <div className="media-container">
+          <div className="media">
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/100x150"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/100x150"
+                alt="Placeholder"
+              />
+            </div>
+            <div className="thumbnail">
+              <img
+                src="https://source.unsplash.com/random/150x100"
+                alt="Placeholder"
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default MediaViewer;
