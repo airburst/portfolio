c89fef02c473ad50606e0d6827e11eafac8a0bf2
airburst <mark@fairhursts.net>
2018-08-24 09:52:37 +0100

    feat: Added ken burns effect to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import './Home.css';
+import './Kenburns.css';
 
-const Home = () => <div className="page-container" />;
+const Home = () => (
+  <div className="page-container">
+    <div className="slideshow">
+      <div
+        className="slideshow-image"
+        style={{
+          backgroundImage:
+            "url('https://source.unsplash.com/category/nature/1600x1400')",
+        }}
+      />
+      <div
+        className="slideshow-image"
+        style={{
+          backgroundImage:
+            "url('https://source.unsplash.com/category/buildings/1600x1400')",
+        }}
+      />
+      <div
+        className="slideshow-image"
+        style={{
+          backgroundImage:
+            "url('https://source.unsplash.com/category/food/1600x1400')",
+        }}
+      />
+      <div
+        className="slideshow-image"
+        style={{
+          backgroundImage:
+            "url('https://source.unsplash.com/category/technology/1600x1400')",
+        }}
+      />
+    </div>
+  </div>
+);
 
 export default Home;
