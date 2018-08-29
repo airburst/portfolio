7c6bfdd4a9aff044cc69de9a7be21a18550a3da4
airburst <mark@fairhursts.net>
2018-08-29 09:03:41 +0100

    feat: Add route guarding

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,52 @@
 import React from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import decode from 'jwt-decode';
+
 import Home from './components/Home';
 import Login from './components/Login';
 import Header from './components/Header';
 import Manager from './components/Manager';
-import FileUpload from './components/Manager/MediaViewer/FileUpload';
 import './index.css';
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  const refreshToken = localStorage.getItem('refreshToken');
+  try {
+    decode(token);
+    const { exp } = decode(refreshToken);
+    if (Date.now() / 1000 > exp) {
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/login',
+          }}
+        />
+      )
+    }
+  />
+);
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
     <Route path="/manager" exact component={Manager} />
-    <Route path="/upload" exact component={FileUpload} />
+    {/* <PrivateRoute path="/manager" exact component={Manager} /> */}
   </Switch>
 );
 
